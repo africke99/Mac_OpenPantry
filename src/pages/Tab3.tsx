ee3b049247585e7d1eb4558e111744dd34475998
Grace <[xlin@macalester.edu]>
2020-11-21 14:12:37 -0600

    worked on adding buttons next to item and showing message

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAlert, IonButton, IonList } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+
+// export interface Product {
+//   name: string; 
+//   weightOz: number; 
+//   amount: number; 
+
+// }
+
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle> My Cart </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      {/* <IonList style= "text-align:center"> */}
+        <section>
+          <header>Block Width</header>
+          <IonButton slot = "middle" size= "small">Check Out</IonButton>
+        </section>
+        {/* </IonList> */}
+
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 3</IonTitle>
