26f7573bfd6c12009f8ed02ee5690d0b99b6961d
lilac <lilacrave01@gmail.com>
2020-11-21 21:55:29 -0600

    trying to make edits to json file via tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,34 @@
+
 import React, { useState } from 'react';
 import { IonContent, IonHeader, IonItem, IonLabel, IonNote, IonPage, IonTitle, IonToolbar, IonList } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import NumberList from '../components/NumberList';
+import fs = require("fs");
+import { readFileSync } from "fs";
+import { writeFile } from "fs";
+
+//import * as inventory from './inventory.json';
+
+//declare function require(path: string):any;
+//function changeInventory(food: string, weight: number){
+//    var subVal = "";
+//    for (var spec in inventory.canned){
+//        if(spec==food){
+//            let spec = Object.create({'name':})
+//        }
+//    }
+//}
+
+var invenContent = readFileSync('inventory.json');
+var words = JSON.parse(invenContent);
+
+function changeInventory(food: string){
+
+    words[food] = 1;
+    var data = JSON.stringify(words);
+    fs.writeFile('inventory.json',data);
+}
 
 function  generateNumList(number: number) { 
 }
@@ -28,7 +54,7 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        
+        <button onClick={this.changeInventory("test")}>test button</button>
         <p className="ion-padding-start ion-padding-end"> </p>
         <NumberList itemName={food} ></NumberList>
         <p className="ion-padding-start ion-padding-end"></p>
