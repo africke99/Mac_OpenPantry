b1a4c939efe7f34ffe5b5179cddbb892cbaf0a72
Augusta <africke@macalester.edu>
2020-12-15 14:50:57 -0600

    still trying

--- static/js/53.42ff6f24.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppantryApp=this.webpackJsonppantryApp||[]).push([[53],{150:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(16),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.42ff6f24.chunk.js.map
