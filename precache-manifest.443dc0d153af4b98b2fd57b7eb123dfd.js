9d02cbe487f7aece68bee3c6a77135c8c846986b
Augusta <africke@macalester.edu>
2020-12-15 14:12:24 -0600

    deploying

---
+++ precache-manifest.443dc0d153af4b98b2fd57b7eb123dfd.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f521e356a9699b878e763383e2c0c2e",
+    "url": "/Mac_OpenPantry.github.io/index.html"
+  },
+  {
+    "revision": "2dee0ece1606813f998a",
+    "url": "/Mac_OpenPantry.github.io/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "33f27160668be7d80402",
+    "url": "/Mac_OpenPantry.github.io/static/css/main.4c81c470.chunk.css"
+  },
+  {
+    "revision": "682be390da744e658c3e",
+    "url": "/Mac_OpenPantry.github.io/static/js/0.098202ac.chunk.js"
+  },
+  {
+    "revision": "42d16a420e4b5e29f39c",
+    "url": "/Mac_OpenPantry.github.io/static/js/10.a7596f6b.chunk.js"
+  },
+  {
+    "revision": "6d3fc8477fbeefeb9835",
+    "url": "/Mac_OpenPantry.github.io/static/js/11.0279b939.chunk.js"
+  },
+  {
+    "revision": "6a592b1a98074b2c2f21",
+    "url": "/Mac_OpenPantry.github.io/static/js/12.c5a3c475.chunk.js"
+  },
+  {
+    "revision": "b6a2b2286a6bad1762d0",
+    "url": "/Mac_OpenPantry.github.io/static/js/13.3993ad66.chunk.js"
+  },
+  {
+    "revision": "2619e487f356d89d7752",
+    "url": "/Mac_OpenPantry.github.io/static/js/14.a988de9b.chunk.js"
+  },
+  {
+    "revision": "bf95acddc394c71f7871",
+    "url": "/Mac_OpenPantry.github.io/static/js/15.0d44f65a.chunk.js"
+  },
+  {
+    "revision": "227bc22833f29d6a2583",
+    "url": "/Mac_OpenPantry.github.io/static/js/16.ca365ca3.chunk.js"
+  },
+  {
+    "revision": "62a4c9be93b294eb435c",
+    "url": "/Mac_OpenPantry.github.io/static/js/17.84cb7f25.chunk.js"
+  },
+  {
+    "revision": "dc5d7ba34deb50e912c9",
+    "url": "/Mac_OpenPantry.github.io/static/js/18.82062ebb.chunk.js"
+  },
+  {
+    "revision": "ed173e6f5cdbae637cd1",
+    "url": "/Mac_OpenPantry.github.io/static/js/19.08e41070.chunk.js"
+  },
+  {
+    "revision": "a1c338e5c8bf1216c738",
+    "url": "/Mac_OpenPantry.github.io/static/js/20.adc4ebce.chunk.js"
+  },
+  {
+    "revision": "fe9cc600419d6a6c8060",
+    "url": "/Mac_OpenPantry.github.io/static/js/21.0c048772.chunk.js"
+  },
+  {
+    "revision": "f4fdeb584968e5a540f7",
+    "url": "/Mac_OpenPantry.github.io/static/js/22.58155bf8.chunk.js"
+  },
+  {
+    "revision": "3e9a5cc40f47e21f9d91",
+    "url": "/Mac_OpenPantry.github.io/static/js/23.3a573a7b.chunk.js"
+  },
+  {
+    "revision": "c6d43301620b032b7288",
+    "url": "/Mac_OpenPantry.github.io/static/js/24.72298f3f.chunk.js"
+  },
+  {
+    "revision": "93e60ba59e95f3c6cf24",
+    "url": "/Mac_OpenPantry.github.io/static/js/25.36afcd2e.chunk.js"
+  },
+  {
+    "revision": "4da35176af5a0fb59efd",
+    "url": "/Mac_OpenPantry.github.io/static/js/26.bef0c2f5.chunk.js"
+  },
+  {
+    "revision": "a24fef671bb84d5263c3",
+    "url": "/Mac_OpenPantry.github.io/static/js/27.16591bd9.chunk.js"
+  },
+  {
+    "revision": "490195951ff54a257c6a",
+    "url": "/Mac_OpenPantry.github.io/static/js/28.c2006d0a.chunk.js"
+  },
+  {
+    "revision": "74e6c2c9d370af8b76f4",
+    "url": "/Mac_OpenPantry.github.io/static/js/29.3d4d4ec3.chunk.js"
+  },
+  {
+    "revision": "eac58b548e4c8912de2f",
+    "url": "/Mac_OpenPantry.github.io/static/js/30.669be6c3.chunk.js"
+  },
+  {
+    "revision": "bb0d44d4be2e5f973e43",
+    "url": "/Mac_OpenPantry.github.io/static/js/31.d993ccad.chunk.js"
+  },
+  {
+    "revision": "e0fbfb74f01c77b77750",
+    "url": "/Mac_OpenPantry.github.io/static/js/32.ea358219.chunk.js"
+  },
+  {
+    "revision": "e425c018c3c290b31e4c",
+    "url": "/Mac_OpenPantry.github.io/static/js/33.aae49377.chunk.js"
+  },
+  {
+    "revision": "792e237cb07873f9fba3",
+    "url": "/Mac_OpenPantry.github.io/static/js/34.73ae4f41.chunk.js"
+  },
+  {
+    "revision": "20a7b7ae44d48355aa12",
+    "url": "/Mac_OpenPantry.github.io/static/js/35.2c136fb2.chunk.js"
+  },
+  {
+    "revision": "97db6f177d582f45f9dc",
+    "url": "/Mac_OpenPantry.github.io/static/js/36.7ba20b63.chunk.js"
+  },
+  {
+    "revision": "1f56111ff7f6aa4c4aac",
+    "url": "/Mac_OpenPantry.github.io/static/js/37.7270f385.chunk.js"
+  },
+  {
+    "revision": "e8844ea84d4f7f5a14c5",
+    "url": "/Mac_OpenPantry.github.io/static/js/38.7f3f1727.chunk.js"
+  },
+  {
+    "revision": "399086281d71ea2d711a",
+    "url": "/Mac_OpenPantry.github.io/static/js/39.b637c103.chunk.js"
+  },
+  {
+    "revision": "2dee0ece1606813f998a",
+    "url": "/Mac_OpenPantry.github.io/static/js/4.e8b62a27.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Mac_OpenPantry.github.io/static/js/4.e8b62a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dad4f024f26521dbf8",
+    "url": "/Mac_OpenPantry.github.io/static/js/40.e23c0fdf.chunk.js"
+  },
+  {
+    "revision": "e3e7e632466de0925ce8",
+    "url": "/Mac_OpenPantry.github.io/static/js/41.32fea4ff.chunk.js"
+  },
+  {
+    "revision": "641336dba22c8394371d",
+    "url": "/Mac_OpenPantry.github.io/static/js/42.cd5e59a5.chunk.js"
+  },
+  {
+    "revision": "71f9089f9095c6d7eeb5",
+    "url": "/Mac_OpenPantry.github.io/static/js/43.976c265e.chunk.js"
+  },
+  {
+    "revision": "5c1def9a1ceb67070cd4",
+    "url": "/Mac_OpenPantry.github.io/static/js/44.da1819a9.chunk.js"
+  },
+  {
+    "revision": "f6f792eaccacfbcb76d8",
+    "url": "/Mac_OpenPantry.github.io/static/js/45.08ddc607.chunk.js"
+  },
+  {
+    "revision": "cbda5d46b942adbb8f6e",
+    "url": "/Mac_OpenPantry.github.io/static/js/46.6100b50f.chunk.js"
+  },
+  {
+    "revision": "e90e9d4871b0fbf71ca0",
+    "url": "/Mac_OpenPantry.github.io/static/js/47.6b62f9ab.chunk.js"
+  },
+  {
+    "revision": "549fed6f5f77da09a18d",
+    "url": "/Mac_OpenPantry.github.io/static/js/48.08291989.chunk.js"
+  },
+  {
+    "revision": "89ee36c8e68961b2033a",
+    "url": "/Mac_OpenPantry.github.io/static/js/49.ef55ea8f.chunk.js"
+  },
+  {
+    "revision": "ca66f5955bd7e72e5c96",
+    "url": "/Mac_OpenPantry.github.io/static/js/5.a0569f57.chunk.js"
+  },
+  {
+    "revision": "b2f1114c761d95983423",
+    "url": "/Mac_OpenPantry.github.io/static/js/50.b325b127.chunk.js"
+  },
+  {
+    "revision": "004268676fd712cbaff5",
+    "url": "/Mac_OpenPantry.github.io/static/js/51.2df2b4a9.chunk.js"
+  },
+  {
+    "revision": "83f693c868aa2f617aa8",
+    "url": "/Mac_OpenPantry.github.io/static/js/52.02444d18.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Mac_OpenPantry.github.io/static/js/52.02444d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19e0709b54ff034242e",
+    "url": "/Mac_OpenPantry.github.io/static/js/53.42ff6f24.chunk.js"
+  },
+  {
+    "revision": "0e0a136e8b09331afacc",
+    "url": "/Mac_OpenPantry.github.io/static/js/54.69456fbe.chunk.js"
+  },
+  {
+    "revision": "4a39c2e42c75fa118db2",
+    "url": "/Mac_OpenPantry.github.io/static/js/55.7054a2e8.chunk.js"
+  },
+  {
+    "revision": "6d3bf50b4d73d0bd7a5d",
+    "url": "/Mac_OpenPantry.github.io/static/js/56.0abd92af.chunk.js"
+  },
+  {
+    "revision": "51bfbe6cc80c6c93976f",
+    "url": "/Mac_OpenPantry.github.io/static/js/57.bb0c2a72.chunk.js"
+  },
+  {
+    "revision": "3cc8a2611e4a15df3317",
+    "url": "/Mac_OpenPantry.github.io/static/js/58.a724a796.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Mac_OpenPantry.github.io/static/js/58.a724a796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999b9553db449de2a55f",
+    "url": "/Mac_OpenPantry.github.io/static/js/6.6f27d570.chunk.js"
+  },
+  {
+    "revision": "9aab26c80f956f775225",
+    "url": "/Mac_OpenPantry.github.io/static/js/7.753701a6.chunk.js"
+  },
+  {
+    "revision": "fa10bd59b78886e640b4",
+    "url": "/Mac_OpenPantry.github.io/static/js/8.034fec08.chunk.js"
+  },
+  {
+    "revision": "7e934315c84a1aab7910",
+    "url": "/Mac_OpenPantry.github.io/static/js/9.9310a02e.chunk.js"
+  },
+  {
+    "revision": "33f27160668be7d80402",
+    "url": "/Mac_OpenPantry.github.io/static/js/main.d3bd7e01.chunk.js"
+  },
+  {
+    "revision": "85bbc343aae75772ddb5",
+    "url": "/Mac_OpenPantry.github.io/static/js/polyfills-dom.8be3df11.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Mac_OpenPantry.github.io/static/js/polyfills-dom.8be3df11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe5c54f2351f65e7f9e",
+    "url": "/Mac_OpenPantry.github.io/static/js/runtime-main.36868167.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Mac_OpenPantry.github.io/precache-manifest.443dc0d153af4b98b2fd57b7eb123dfd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mac_OpenPantry.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
